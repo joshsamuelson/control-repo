460a77fd6ec66a75270fdaaaa16cce917a2b7cd0
Josh Samuelson <josh@joshsamuelson.com>
2017-09-19 22:23:26 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,7 +5,7 @@ class minecraft {
   include wget
   wget::fetch { "download minecraft":
     source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
-    destination => '/opt/minecraft',
+    destination => '/tmp',
     timeout     => 0,
     verbose     => false,
   }
