d4d5973230b2bc279f9ad3c324f4dbd02537a169
Josh Samuelson <josh@joshsamuelson.com>
2017-09-19 20:41:26 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,7 +10,7 @@ node 'master.puppet.vm' {
   include role::master_server
   file {'/root/README':
     ensure => file,
-    content => $fqdn,
+    content => "Welcome to ${fqdn}",
     owner => 'root',
   }
 }
