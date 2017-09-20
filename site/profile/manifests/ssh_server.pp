2aa2b830956fda66eaa9ec030186a86eade5b08d
Josh Samuelson <josh@joshsamuelson.com>
2017-09-19 20:52:43 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDujh4shDdchlRzZJ1Bn8eBXhFB2sE9cm32VOPyrp8Er9ireIAbiq3cv2FyR9VS7pqAIRBmNO6ZETLmuR7tD0VEcEj6DB0jgLYeRZecwgKVRk5XqYUNB/wsDvBqcYCOvDX4con3ltAzHOheucu/RQJdFFcvfIxGpbA8dvQ0X4otD6y3g614KCkeLYZLymdWUlaIabcCgWP4sSiuP6YKtkggi9uID+csgGv8jiFn49kFs7BEFJXXl6R3P5T/0mKJMxjyfC3+btrVzDfXbvwGomMvWhCuYySZuqEhlwqapbnGRGtAXD9zpOhmT+QbfCcO9/tqMV7eKaVuMEbcEZ0yo7Z root@master.puppet.vm',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDDujh4shDdchlRzZJ1Bn8eBXhFB2sE9cm32VOPyrp8Er9ireIAbiq3cv2FyR9VS7pqAIRBmNO6ZETLmuR7tD0VEcEj6DB0jgLYeRZecwgKVRk5XqYUNB/wsDvBqcYCOvDX4con3ltAzHOheucu/RQJdFFcvfIxGpbA8dvQ0X4otD6y3g614KCkeLYZLymdWUlaIabcCgWP4sSiuP6YKtkggi9uID+csgGv8jiFn49kFs7BEFJXXl6R3P5T/0mKJMxjyfC3+btrVzDfXbvwGomMvWhCuYySZuqEhlwqapbnGRGtAXD9zpOhmT+QbfCcO9/tqMV7eKaVuMEbcEZ0yo7Z',
   }  
 }
