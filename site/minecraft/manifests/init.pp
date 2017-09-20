5ae8cb02bf251a8f96b0c8daed4cf57a310b713d
Josh Samuelson <josh@joshsamuelson.com>
2017-09-19 22:59:37 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,27 +1,27 @@
 class minecraft (
   $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
-  $install_dir = ‘/opt/minecraft’
+  $install_dir = '/opt/minecraft'
 ){
   file { $install_dir:
     ensure => directory,
   }
-  file { “${install_dir}/minecraft_server.jar:
+  file { "${install_dir}/minecraft_server.jar":
     ensure => file,
     source => $url,
   }
 
-  package {‘java’:
+  package {'java':
     ensure => present,
   }
-  file {“${install_dir}/eula.txt”:
+  file {"${install_dir}/eula.txt":
     ensure => file,
-    content => ‘eula=true’
+    content => 'eula=true'
   }
-  file {‘/etc/systemd/system/minecraft.service’:
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => ‘puppet:///modules/minecraft/minecraft.service’,
+    source => 'puppet:///modules/minecraft/minecraft.service',
   }
-  service { ‘minecraft’:
+  service { 'minecraft':
     ensure => running,
     enable => true,
   }
