6a47189b79f20db69991de9ffe7715b9230d865b
Josh Samuelson <josh@joshsamuelson.com>
2017-09-19 21:38:22 -0700

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,6 +2,7 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm': }
   dockeragent::node {'db.puppet.vm': }
+  dockeragent::node {'minetest.puppet.vm':}
   host {'web.puppet.vm':
     ensure => present,
     ip => '172.18.0.2',
