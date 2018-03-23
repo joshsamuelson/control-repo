7e2fc7ecd474d868990cb8a6faa5e72bb12e741d
Josh Samuelson <josh@joshsamuelson.com>
2018-03-23 14:56:39 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,12 +2,15 @@ node default {
 
 }
 node master {
+  class { "r10k":
+    remote => "https://github.com/samuelson/control_repo"
+  }
   class {'r10k::webhook::config':
-  enable_ssl     => false,
-  protected      => false,
-}
+    enable_ssl     => false,
+    protected      => false,
+  }
 
-class {'r10k::webhook':
-  require => Class['r10k::webhook::config'],
-}
+  class {'r10k::webhook':
+    require => Class['r10k::webhook::config'],
+  }
 }
