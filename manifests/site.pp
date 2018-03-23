0ac9fc6c57408830fa75a86945bc558ef7667098
Josh Samuelson <josh@joshsamuelson.com>
2018-03-23 15:08:05 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,13 +5,18 @@ node puppet.linkedin.local {
   class { "r10k":
     remote => "https://github.com/samuelson/control_repo"
   }
-  class {'r10k::webhook::config':
-    use_mcollective => false,
-    enable_ssl     => false,
-    protected      => false,
-  }
+# Instead of running via mco, run r10k directly
+class {'r10k::webhook::config':
+  use_mcollective => false,
+}
 
-  class {'r10k::webhook':
-    require => Class['r10k::webhook::config'],
-  }
+# The hook needs to run as root when not running using mcollective
+# It will issue r10k deploy environment <branch_from_gitlab_payload> -p
+# When git pushes happen.
+class {'r10k::webhook':
+  use_mcollective => false,
+  user            => 'root',
+  group           => '0',
+  require         => Class['r10k::webhook::config'],
+}
 }
