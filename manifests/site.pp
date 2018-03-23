7e8dfab5ad260f0fa4ed948c0151e3248b9d1bee
Josh Samuelson <josh@joshsamuelson.com>
2018-03-23 14:55:41 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,13 @@
 node default {
 
 }
+node master {
+  class {'r10k::webhook::config':
+  enable_ssl     => false,
+  protected      => false,
+}
+
+class {'r10k::webhook':
+  require => Class['r10k::webhook::config'],
+}
+}
