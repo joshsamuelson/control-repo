89c48cebee6f04e52f278e9fc7149b81b8d62124
Josh Samuelson <josh@joshsamuelson.com>
2018-03-23 18:21:49 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,17 +2,5 @@ node default {
 
 }
 node puppet.local {
-  class { "r10k":
-    remote => "https://github.com/samuelson/control_repo"
-  }
 
-class { 'r10k::webhook::config':
-  use_mcollective  => false,
-  enable_ssl => false,
-}
-
-class { 'r10k::webhook':
-  user    => 'root',                                       # Mandatory for FOSS
-  group   => 'root',                                       # Mandatory for FOSS
-}
 }
