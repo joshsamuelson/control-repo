6179233ff88a10ac3acf5ee67af34cfef80e3e4e
Josh Samuelson <josh@joshsamuelson.com>
2018-03-23 18:21:55 -0700

    Create r10k.pp

---
+++ profile/manifests/r10k.pp
@@ -0,0 +1,15 @@
+class profile::r10k {
+  class { "r10k":
+    remote => "https://github.com/samuelson/control_repo"
+  }
+
+class { 'r10k::webhook::config':
+  use_mcollective  => false,
+  enable_ssl => false,
+}
+
+class { 'r10k::webhook':
+  user    => 'root',                                       # Mandatory for FOSS
+  group   => 'root',                                       # Mandatory for FOSS
+}
+}
