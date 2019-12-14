3ed2fe2cbd48e94a27a83c5c8e77a6395b3e3e9f
Josh <josh@example.com>
2019-12-14 13:21:24 -0800

    Update repo

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,15 +1,13 @@
 class profile::r10k {
-  class { "r10k":
-    remote => "https://github.com/samuelson/control_repo"
+  class {'r10k':
+    remote => 'https://github.com/joshsamuelson/control-repo',
   }
-
-  class { 'r10k::webhook::config':
-    use_mcollective  => false,
-    enable_ssl => false,
-  }  
-
-  class { 'r10k::webhook':
-    user    => 'root',                                       # Mandatory for FOSS
-    group   => 'root',                                       # Mandatory for FOSS
+  class {'r10k::webhook::config':
+    use_mcollective => false,
+    enable_ssl      => false,
+  }
+  class {'r10k::webhook':
+    user  => 'root',
+    group => 'root',
   }
 }
