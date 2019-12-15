277d1a41964e4181bceeba06f00d907355da6f47
joshsamuelson <32172285+joshsamuelson@users.noreply.github.com>
2019-12-14 23:00:35 -0800

    Create puppetdb.pp

---
+++ site/profile/manifests/puppetdb.pp
@@ -0,0 +1,6 @@
+class profile::puppetdb {
+  # Configure puppetdb and its underlying database
+  class { 'puppetdb': }
+  # Configure the Puppet master to use puppetdb
+  class { 'puppetdb::master::config': }
+}
